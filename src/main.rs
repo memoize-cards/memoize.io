1255b6c118def90853b589a4949bdbef56376763
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-27 09:58:04 -0300

    refactor(html): inclusao da possibilidade do uso do data attribute

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let document = html!(
         <div class="avatar">
             <img alt={name} src="./deMGoncalves.png" />
-            <strong>"I'm deMGoncalves"</strong>
+            <strong>{format!("I'm {name}")}</strong>
         </div>
     );
     println!("{}", document);
