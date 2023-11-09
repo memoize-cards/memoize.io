f48bf858b4b91aeb0bc6360076d2f723d987c02e
deMGoncalves <cleber.engineer@hotmail.com>
2023-11-09 08:38:18 -0300

    ci: configuracao do projeto

--- .wrangler/tmp/bundle-CF2a15/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-CF2a15/middleware-loader.entry.ts
@@ -11,10 +11,10 @@ import {
 	__facade_register__,
 } from "/usr/local/lib/node_modules/wrangler/templates/middleware/common.ts";
 
-import worker from "/home/clebergoncalves/Workspace/memoize.io/.wrangler/tmp/bundle-HSfZx8/middleware-insertion-facade.js";
+import worker from "/home/clebergoncalves/Workspace/memoize.io/.wrangler/tmp/bundle-CF2a15/middleware-insertion-facade.js";
 
 // We need to preserve all of the exports from the worker
-export * from "/home/clebergoncalves/Workspace/memoize.io/.wrangler/tmp/bundle-HSfZx8/middleware-insertion-facade.js";
+export * from "/home/clebergoncalves/Workspace/memoize.io/.wrangler/tmp/bundle-CF2a15/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	#noRetry: ScheduledController["noRetry"];
--- .wrangler/tmp/dev-PaGkFF/shim.js
+++ .wrangler/tmp/dev-PaGkFF/shim.js
@@ -1,4 +1,4 @@
-// .wrangler/tmp/bundle-HSfZx8/checked-fetch.js
+// .wrangler/tmp/bundle-CF2a15/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -608,7 +608,7 @@ var jsonError = async (request, env, _ctx, middlewareCtx) => {
 var middleware_miniflare3_json_error_default = jsonError;
 var wrap = void 0;
 
-// .wrangler/tmp/bundle-HSfZx8/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-CF2a15/middleware-insertion-facade.js
 var envWrappers = [wrap].filter(Boolean);
 var facade = {
   ...Yt,
@@ -620,7 +620,7 @@ var facade = {
 };
 var middleware_insertion_facade_default = facade;
 
-// .wrangler/tmp/bundle-HSfZx8/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-CF2a15/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
