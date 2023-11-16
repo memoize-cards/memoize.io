34fa0211248807314c3c4bd5d15a0a5e5b7fda04
deMGoncalves <cleber.engineer@hotmail.com>
2023-11-16 19:56:48 -0300

    test: teste de implementacao de uma forma simples de montar paginas

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,56 @@
-use css::*;
-use html::*;
+use std::fmt;
 use worker::*;
+
+// Defina um componente reutilizável
+struct Header<'a> {
+    title: &'a str,
+}
+
+// Implemente a trait Display para o componente
+impl<'a> fmt::Display for Header<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "<head><title>{}</title></head>", self.title)
+    }
+}
+
+// Struct principal que incorpora o componente Header
+struct MyPage<'a> {
+    header: Header<'a>,
+    content: &'a str,
+}
+
+// Implemente a trait Display para a struct principal
+impl<'a> fmt::Display for MyPage<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "<html>{}<body><h1>{}</h1><p>{}</p></body></html>",
+            self.header, self.header.title, self.content
+        )
+    }
+}
+
+// Implemente métodos para manipular a struct principal
+impl<'a> MyPage<'a> {
+    // Construtor
+    fn new(title: &'a str, content: &'a str) -> MyPage<'a> {
+        let header = Header { title };
+        MyPage { header, content }
+    }
+
+    // Método para renderizar o template
+    fn render(&self) -> String {
+        format!("{}", self)
+    }
+}
 
 #[event(fetch)]
 async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
-    let body: String = html!(
-        <html>
-            <head>
-                <meta charset="UTF-8" />
-                <title>{"Memoize"}</title>
-                {css::style()}
-            </head>
-            <body>
-                <div class="avatar">
-                    <img alt="Memoize" src="./memoize.svg" />
-                </div>
-            </body>
-        </html>
-    );
+    // Crie uma instância da struct principal
+    let my_page = MyPage::new("Minha Página", "Conteúdo da página.");
 
-    Response::from_html(body)
-}
+    // Chame o método render para obter o template
+    let template = my_page.render();
 
-#[event(start)]
-fn start() {
-    css(r#"
-        * {
-            margin: 0;
-            padding: 0;
-        }
-    "#);
+    Response::from_html(format!("{}", template))
 }
