e3618c392859dc9f493ab944273b425c32638f2e
deMGoncalves <cleber.engineer@hotmail.com>
2023-11-25 22:13:34 -0300

    refactor(reset): assinatura do mod css mudou

--- src/reset.rs
+++ src/reset.rs
@@ -1,22 +1,8 @@
-//! This module provides functions for manipulating CSS styles in the application.
+use crate::css;
 
-use crate::css::*;
-
-/// Initializes the default CSS style for the application.
-///
-/// This function applies common styles to all elements, such as `box-sizing`,
-/// `margin`, and `padding`, and sets the background color of the body.
-///
-/// # Example
-///
-/// ```rust
-/// use crate::css::init;
-///
-/// // Initialize the CSS style
-/// init();
-/// ```
 pub fn init() {
-    css(r#"
+    css::push(
+        r#"
         *,
         *::after,
         *::before {
@@ -28,5 +14,6 @@ pub fn init() {
         body {
             background-color: var(--color-master-lightest);
         }
-    "#);
+    "#,
+    );
 }
