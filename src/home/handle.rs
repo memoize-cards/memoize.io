3cc2631c8f905bbe53d174b1a0102f6a481737e1
deMGoncalves <cleber.engineer@hotmail.com>
2023-11-25 22:11:17 -0300

    refactor(home.handle): o home que precisa se construir, apenas receber suas dependencias

--- src/home/handle.rs
+++ src/home/handle.rs
@@ -1,49 +1,15 @@
-//! # Handler Module
-//!
-//! The `handler` module contains functionality related to request handling.
-
 use super::Home;
 use crate::handler::Handler;
 use crate::head::Head;
 use worker::*;
 
 impl<'a> Handler for Home<'a> {
-    /// Implements the `Handler` trait for the `Home` struct, defining how requests to the home route should be handled.
-    ///
-    /// # Arguments
-    ///
-    /// - `_req`: The incoming request.
-    /// - `_ctx`: The route context.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing the generated `Response`.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use your_crate_name::{Home, Handler};
-    /// use worker::{Request, RouteContext, Result, Response};
-    ///
-    /// // Simulate incoming request and route context.
-    /// let req: Request = /* create request */;
-    /// let ctx: RouteContext<_> = /* create route context */;
-    ///
-    /// // Handle the request using the `Home` struct.
-    /// let result = Home::handle(req, ctx);
-    /// // Process the result, e.g., send the response to the client.
-    /// ```
     fn handle<T>(_req: Request, _ctx: RouteContext<T>) -> Result<Response> {
-        // Create a `Head` instance with title and description.
-        let head: Head = Head {
+        let head = Head {
             title: "Memoize",
             description: "Aprenda, Memorize, Domine!",
         };
-
-        // Create a `Home` instance with the generated `Head`.
-        let home: Home = Home { head: &head };
-
-        // Format the `Home` instance as HTML and create a `Response`.
+        let home = Home::new(&head);
         Response::from_html(format!("{home}"))
     }
 }
