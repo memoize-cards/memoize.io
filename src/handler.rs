9362434b084cb74b0154fd4dfcd502f7c7e950da
deMGoncalves <cleber.engineer@hotmail.com>
2023-11-25 22:08:32 -0300

    refactor(handler): removi os comentarios

--- src/handler.rs
+++ src/handler.rs
@@ -1,20 +1,5 @@
-//! # Handler Trait
-//!
-//! The `Handler` trait defines the interface for request handling.
-
 use worker::*;
 
-/// A trait for handling HTTP requests.
 pub trait Handler {
-    /// Handles an HTTP request.
-    ///
-    /// # Arguments
-    ///
-    /// * `req` - The HTTP request.
-    /// * `ctx` - The context for routing.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing the HTTP response.
     fn handle<T>(req: Request, ctx: RouteContext<T>) -> Result<Response>;
 }
