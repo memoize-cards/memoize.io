d8a4ebd21b733679d040e25c389c9a3db9ad8b84
deMGoncalves <cleber.engineer@hotmail.com>
2023-11-25 21:58:07 -0300

    refactor(css): simplificando a estrutura do codigo

--- src/css.rs
+++ src/css.rs
@@ -1,67 +1,34 @@
-//! # CSS Module
-//!
-//! The `css` module provides functionality for handling CSS content.
-
-/// Stores the CSS content as a mutable static string.
 static mut CONTENT: String = String::new();
 
-/// Processes and stores the provided CSS content.
-///
-/// # Arguments
-///
-/// * `content` - A string containing CSS content.
-///
-/// # Returns
-///
-/// The processed CSS content.
-pub fn css(content: &str) -> String {
-    // Remove newline characters and collapse consecutive whitespace into a single space
+pub fn get_content() -> String {
+    unsafe { CONTENT.clone() }
+}
+
+pub fn push(content: &str) {
     let content: String = content
         .replace('\n', " ")
         .split_whitespace()
         .collect::<Vec<&str>>()
         .join(" ");
 
-    // Use unsafe to modify the static CONTENT string
     unsafe {
         CONTENT.push_str(&content);
     }
-
-    content
-}
-
-/// Generates an HTML style element with the stored CSS content.
-///
-/// # Returns
-///
-/// The HTML style element string.
-pub fn style() -> String {
-    // Use unsafe to access the static CONTENT string
-    unsafe { format!("<style>{}</style>", CONTENT) }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::css;
 
     #[test]
-    fn test_content_processing() {
-        let style: String = css(r#"
+    fn push_and_get_content() {
+        css::push(
+            r#"
             * {
                 all: unset;
             }
-        "#);
-        assert_eq!(style, "* { all: unset; }");
-    }
-
-    #[test]
-    fn test_html_style_element_generation() {
-        css(r#"
-                * {
-                all: unset;
-            }
-        "#);
-        let tag: String = style();
-        assert_eq!(tag, "<style>* { all: unset; }</style>")
+        "#,
+        );
+        assert_eq!(css::get_content(), "* { all: unset; }");
     }
 }
